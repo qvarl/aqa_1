729199741989a4e4b7eaa0892dea9a9e450dbe7d
zero <viktorkin0511@gmail.com>
2023-10-08 19:47:57 +0300

    Create test

---
+++ src/test/java/github/SearchForSelenide.java
@@ -0,0 +1,32 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchForSelenide {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = "edge";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void shouldFindJUnit5CodeExample() {
+        open("/selenide/selenide");
+
+        $("#wiki-tab").click();
+
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).click();
+
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
